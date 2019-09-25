594b8e6f96db48f49d792da27ec694eb6ef589fd
Bryce Braden <brycecbraden@gmail.com>
2019-09-25 16:49:03 -0500

    Basic JavaScript Projects upload
    window alerts, document write, var, strings, concatenations etc

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,31 @@
+window.alert("Hello, World!");
+
+var A = "This is a string";
+
+document.write(A);
+
+window.alert(A);
+
+//var B = ("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>
+//    \"Eat my shorts!\" Bart responded.";
+
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ " Dr. Seuss");
+
+var C = "Concatenated" + " String";
+document.write(C);
+
+var D = "Orange"
+  , E = ""
+  , F = ""
+  , G = "";
+
+var D = D.fontcolor("orange")
+  , E = ""
+  , F = ""
+  , G = "";
+
+document.write(D);
+
+document.write(3 + 3);
